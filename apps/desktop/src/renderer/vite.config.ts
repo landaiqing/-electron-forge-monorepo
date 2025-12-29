72b7915b8b0b023d879b9e28d5fbe1f814d735e6
landaiqing <landaiqing@126.com>
2025-12-29 17:51:42 +0800

    :bug: fixed path error issues

--- apps/desktop/src/renderer/vite.config.ts
+++ apps/desktop/src/renderer/vite.config.ts
@@ -1,14 +1,7 @@
 import {defineConfig} from 'vite';
-import path from 'path';
 
 // https://vitejs.dev/config
 export default defineConfig({
     root: __dirname,
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@craft-studio/ui': path.resolve(__dirname, '../../../../packages/ui/src'),
-        },
-    },
 });
 
