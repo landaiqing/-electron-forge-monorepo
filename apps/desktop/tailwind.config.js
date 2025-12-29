e51b4c12a6b53001caccb3dce1df902ea825e338
landaiqing <landaiqing@126.com>
2025-12-30 00:30:54 +0800

    :lipstick: update style import and adjust configuration content

--- apps/desktop/tailwind.config.js
+++ apps/desktop/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './src/renderer/src/**/*.{js,ts,jsx,tsx}',
-        '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
-    ],
-} ;
+  content: [
+    './src/renderer/src/**/*.{js,ts,jsx,tsx}',
+    './src/renderer/src/**/*.{css}',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/src/**/*.{css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
