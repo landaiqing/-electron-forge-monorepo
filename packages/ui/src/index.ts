b64657780959c6bf8e345fb91045473f62499e9b
landaiqing <landaiqing@126.com>
2025-12-29 18:07:40 +0800

    :bug: fixed dependency import issues

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,12 +1,3 @@
 // UI 组件库入口
-
-// Utils
 export * from './lib/utils'
 
-// Components
-export * from './components/button'
-export * from './components/card'
-export * from './components/field'
-export * from './components/input'
-export * from './components/label'
-export * from './components/separator'
